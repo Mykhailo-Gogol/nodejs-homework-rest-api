9ab495c21d522ab3833a11f1f8372fb8c0d2da67
mykhailo-gogol <mishhagogol@gmail.com>
2021-06-17 15:35:08 +0300

    connect to mongo database

--- bin/server.js
+++ bin/server.js
@@ -1,8 +1,23 @@
 const app = require('../app')
+const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server running. Use our API on port: ${PORT}`)
-})
+const start = async () => {
+  const client = await MongoClient.connect(process.env.MONGO_CONNECT_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  const db = client.db()
+
+  const ContactsCollection = db.collection('contacts')
+  const contacts = await ContactsCollection.find({}).toArray()
+  console.log(contacts)
+
+  app.listen(PORT, () => {
+    console.log(`Server running. Use our API on port: ${PORT}`)
+  })
+}
+
+start()
