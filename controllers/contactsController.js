78dd7e0edd4f2596f02ae68d9558e15632500ef9
mykhailo-gogol <mishhagogol@gmail.com>
2021-06-21 18:19:02 +0300

    add CRUD, without favorite

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -2,7 +2,6 @@ const { Contact } = require('../db/contactModel')
 const { HttpCode } = require('../heplers/constants')
 
 const getContactsController = async (req, res, next) => {
-  // try {
   const contacts = await Contact.find({})
 
   res.status(HttpCode.OK).json({
@@ -10,97 +9,88 @@ const getContactsController = async (req, res, next) => {
     code: HttpCode.OK,
     data: { contacts }
   })
-  // } catch (error) {
-  //   res.status(HttpCode.NOT_FOUND).json({
-  //     status: HttpCode.NOT_FOUND,
-  //     message: 'Not found'
-  //   })
-  // }
 }
 
 const getContactByIdController = async (req, res, next) => {
-  // try {
-  //   const id = req.params.contactId
-  //   const contact = await req.db.Contacts.findOne({ _id: ObjectId(id) })
-  //   if (contact) {
-  //     res.status(HttpCode.OK).json({
-  //       status: 'success',
-  //       code: HttpCode.OK,
-  //       data: { contact }
-  //     })
-  //   } else {
-  //     return next({
-  //       status: HttpCode.NOT_FOUND,
-  //       message: 'Not found'
-  //     })
-  //   }
-  // } catch (error) {
-  //   next(error)
-  // }
+  const id = req.params.contactId
+  const contact = await Contact.findById(id)
+
+  if (contact) {
+    res.status(HttpCode.OK).json({
+      status: 'success',
+      code: HttpCode.OK,
+      data: { contact }
+    })
+  }
+
+  if (!contact) {
+    res.status(HttpCode.BAD_REQUEST).json({
+      status: HttpCode.BAD_REQUEST,
+      message: 'Not found'
+    })
+  }
 }
 
 const deleteContactByIdController = async (req, res, next) => {
-  // try {
-  //   const id = req.params.contactId
-  //   const contact = await req.db.Contacts.findOne({ _id: ObjectId(id) })
-  //   await req.db.Contacts.deleteOne({ _id: ObjectId(id) })
-  //   if (contact) {
-  //     res.status(HttpCode.OK).json({
-  //       status: 'success',
-  //       code: HttpCode.OK,
-  //       data: { contact }
-  //     })
-  //   } else {
-  //     return next({
-  //       status: HttpCode.NOT_FOUND,
-  //       message: 'Not found'
-  //     })
-  //   }
-  // } catch (error) {
-  //   next(error)
-  // }
+  const id = req.params.contactId
+  const contact = await Contact.findById(id)
+
+  await Contact.findByIdAndDelete(id)
+
+  if (contact) {
+    res.status(HttpCode.OK).json({
+      status: 'success',
+      code: HttpCode.OK,
+      data: { contact }
+    })
+  }
+
+  if (!contact) {
+    res.status(HttpCode.BAD_REQUEST).json({
+      status: HttpCode.BAD_REQUEST,
+      message: 'Not found'
+    })
+  }
 }
 
 const addContactController = async (req, res, next) => {
-  // try {
-  //   const { name, email, phone, favorite } = await req.body
-  //   const newContact = { name, email, phone, favorite }
-  //   await req.db.Contacts.insert(newContact)
-  //   res.status(HttpCode.CREATED).json({
-  //     status: 'success',
-  //     code: HttpCode.CREATED,
-  //     message: 'contact created',
-  //     data: { newContact }
-  //   })
-  // } catch (error) {
-  //   next(error)
-  // }
+  const { name, email, phone, favorite } = await req.body
+
+  const newContact = new Contact({ name, email, phone, favorite })
+  await newContact.save()
+
+  res.status(HttpCode.CREATED).json({
+    status: 'success',
+    code: HttpCode.CREATED,
+    message: 'contact created',
+    data: { newContact }
+  })
 }
 
 const updateContactByIdController = async (req, res, next) => {
-  // try {
-  //   const id = req.params.contactId
-  //   const { name, email, phone, favorite } = req.body
-  //   await req.db.Contacts.updateOne(
-  //     { _id: ObjectId(id) },
-  //     { $set: { name, email, phone, favorite } }
-  //   )
-  //   const contact = await req.db.Contacts.findOne({ _id: ObjectId(id) })
-  //   if (contact) {
-  //     res.status(HttpCode.OK).json({
-  //       status: 'success',
-  //       code: HttpCode.OK,
-  //       data: { contact }
-  //     })
-  //   } else {
-  //     return next({
-  //       status: HttpCode.NOT_FOUND,
-  //       message: 'Not found'
-  //     })
-  //   }
-  // } catch (error) {
-  //   next(error)
-  // }
+  const id = req.params.contactId
+  const { name, email, phone, favorite } = req.body
+
+  await Contact.findByIdAndUpdate(id, {
+    $set: { name, email, phone, favorite }
+  })
+
+  const contact = await Contact.findById(id)
+
+  if (contact) {
+    res.status(HttpCode.OK).json({
+      status: 'success',
+      code: HttpCode.OK,
+      data: { contact }
+    })
+  }
+
+  if (!contact) {
+    res.status(HttpCode.BAD_REQUEST).json({
+      status: HttpCode.BAD_REQUEST,
+      message: 'Not found'
+    })
+  }
 }
 
 // const updateFaforiteByIdController = async (req, res, next) => {
